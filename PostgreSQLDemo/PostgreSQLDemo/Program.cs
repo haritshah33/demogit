e5513f2b3b91a505118af9884bebd93f7d521823
Harit <harits@cloudthat.in>
2017-12-09 00:07:44 +0530

    second commit

---
+++ PostgreSQLDemo/PostgreSQLDemo/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace PostgreSQLDemo
+{
+    class Program
+    {
+       
+             // Obtain connection string information from the portal
+             //
+        private static string Host = "enter server name";
+        private static string User = "enter server admin login name";
+        private static string DBname = "mypgsqldb";
+        private static string Password = "enter-password";
+        private static string Port = "5432";
+
+        static void Main(string[] args)
+        {
+            // Build connection string using parameters from portal
+            //
+            string connString =
+                String.Format(
+                    "Server={0}; User Id={1}; Database={2}; Port={3}; Password={4}; SSL Mode=Prefer; Trust Server Certificate=true",
+                    Host,
+                    User,
+                    DBname,
+                    Port,
+                    Password);
+
+            var conn = new NpgsqlConnection(connString);
+
+            Console.Out.WriteLine("Opening connection");
+            conn.Open();
+
+            var command = conn.CreateCommand();
+            command.CommandText = "DROP TABLE IF EXISTS inventory;";
+            command.ExecuteNonQuery();
+            Console.Out.WriteLine("Finished dropping table (if existed)");
+
+            command.CommandText = "CREATE TABLE inventory (id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER);";
+            command.ExecuteNonQuery();
+            Console.Out.WriteLine("Finished creating table");
+
+            command.CommandText =
+                String.Format(
+                    @"
+                                INSERT INTO inventory (name, quantity) VALUES ({0}, {1});
+                                INSERT INTO inventory (name, quantity) VALUES ({2}, {3});
+                                INSERT INTO inventory (name, quantity) VALUES ({4}, {5});
+                            ",
+                    "\'banana\'", 150,
+                    "\'orange\'", 154,
+                    "\'apple\'", 100
+                    );
+
+            int nRows = command.ExecuteNonQuery();
+            Console.Out.WriteLine(String.Format("Number of rows inserted={0}", nRows));
+
+            var readCommand = conn.CreateCommand();
+            readCommand.CommandText = "SELECT * FROM inventory;";
+
+            var reader = readCommand.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(
+                    string.Format(
+                        "Reading from table=({0}, {1}, {2})",
+                        reader.GetInt32(0).ToString(),
+                        reader.GetString(1),
+                        reader.GetInt32(2).ToString()
+                        )
+                    );
+            }
+
+
+            Console.Out.WriteLine("Closing connection");
+            conn.Close();
+
+            Console.WriteLine("Press RETURN to exit");
+            Console.ReadLine();
+        }
+    }
+}
